5e01e080940f52024dd09d9e55b56a19eb83e415
Eren <eren.g94@gmail.com>
2026-04-10 17:03:26 +0200

    update strunsplit

--- tests/test_tokenize.c
+++ tests/test_tokenize.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 18:34:37 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/04/10 14:42:30 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/04/10 17:03:01 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	test_tokenize(char *line, bool free_input)
 {
 	t_token	*root;
 
+	ft_printf("start tokenize\n");
 	root = tokenize(line);
 	print_tokens(root);
 	free_tokens(root);
