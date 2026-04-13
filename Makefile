5f70ec35bb35978d8a5ab615d641f2f34007293c
jmal423 <jarvasgaming010@gmail.com>
2026-04-13 14:03:03 +0200

    added signal features with a simple REPL minishell

--- Makefile
+++ Makefile
@@ -1,4 +1,23 @@
-all:
-	cc -I/usr/local/include -lreadline main.c
+NAME = minishell
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
 
-.PHONY: all
+RL_INC = -I/opt/homebrew/opt/readline/include
+RL_LIB = -L/opt/homebrew/opt/readline/lib -lreadline
+
+SRCS = signals.c main.c
+
+all: $(NAME)
+
+$(NAME):
+	$(CC) $(CFLAGS) $(SRCS) $(RL_INC) $(RL_LIB) -o $(NAME)
+
+clean:
+	rm -f *.o
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- main.c
+++ main.c
@@ -1,10 +1,69 @@
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/readline.h>
-// #include <readline/history.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_signals.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalfaiat <jalfaiat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 12:54:35 by jalfaiat          #+#    #+#             */
+/*   Updated: 2026/04/13 12:54:36 by jalfaiat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int ac, char **av){
-	(void )ac;
-	printf("buffer = <%s>", readline(av[1]));
-	return 0;
+#include "signals.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(void)
+{
+	char	*line;
+	pid_t	pid;
+
+	while (1)
+	{
+		set_interactive_signals();
+
+		line = readline("minishell> ");
+		if (line == NULL)
+		{
+			printf("exit\n");
+			break;
+		}
+
+		if (strlen(line) > 0)
+			add_history(line);
+
+		// ==========================================
+		// [PLACEHOLDER] 2. TOKENIZATION & PARSING
+		// ==========================================
+
+		if (strncmp(line, "sleep", 5) == 0)
+		{
+			printf("Running command (sleep 5)...\n");
+			printf("Try pressing Ctrl-C or Ctrl-\\ now!\n");
+
+			pid = fork();
+			if (pid == 0)
+			{
+				set_child_signals();
+
+				sleep(5);
+				printf("\nSleep finished without being killed.\n");
+				exit(0);
+			}
+			else if (pid > 0)
+			{
+				set_execution_signals();
+				waitpid(pid, NULL, 0);
+			}
+		}
+
+		free(line);
+	}
+	return (0);
 }
