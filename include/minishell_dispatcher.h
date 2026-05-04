9af9b72ead28e2b2f1e1077269db36352d42e8ea
Eren Gazioglu <eren.g94@gmail.com>
2026-05-04 17:14:36 +0200

    remove commits

--- include/minishell_dispatcher.h
+++ include/minishell_dispatcher.h
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 11:05:21 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/05/04 13:36:16 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/05/04 16:39:09 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,32 +23,6 @@
 
 # define FLAG_READ	0
 # define FLAG_WRITE	1
-
-// typedef struct s_state {
-// 	int		fd[3];
-// 	int		exit_code;
-// 	int		argc;
-// 	char	**argv;
-// 	char	**child_args;
-// 	char	**envp;
-// 	pid_t	pid;
-// }	t_state;
-
-// typedef enum e_err {
-// 	ERR_NONE,
-// 	ERR_ARGS,
-// 	ERR_MALLOC,
-// 	ERR_PIPE,
-// 	ERR_FORK,
-// 	ERR_OPENR,
-// 	ERR_OPENW,
-// 	ERR_DUP2,
-// 	ERR_STR,
-// 	ERR_EXEC,
-// 	ERR_CMDNOTFOUND,
-// 	ERR_CMDDENIED,
-// 	ERR_CMDEMPTY
-// }	t_err;
 
 // dispatch.c
 
