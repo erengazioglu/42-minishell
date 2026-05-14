bbc2579350604af5efe5dc67473bfb04f13bbaf9
Eren Gazioglu <eren.g94@gmail.com>
2026-05-14 21:49:31 +0200

    norminette

--- include/minishell_signals.h
+++ include/minishell_signals.h
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:39:46 by jalfaiat          #+#    #+#             */
-/*   Updated: 2026/05/14 21:42:47 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/05/14 21:49:07 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,5 @@ void	set_heredoc_signals(void);
 void	rl_replace_line(const char *text, int clear_undo);
 void	handle_sigint_interactive(int signum);
 void	handle_sigint_heredoc(int signum);
-
 
 #endif
